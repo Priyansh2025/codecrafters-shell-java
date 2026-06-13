8c3f52d1a142e2e8bb5f62512dc373cab8fe721d
Priyansh2025 <priyansh05khanna@gmail.com>
2026-06-13 23:56:17 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,50 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
-
         Scanner sc = new Scanner(System.in);
+        String path = System.getenv("PATH");
+        String[] pathDirs = path.split(":");
+
         while (true) {
             System.out.print("$ ");
-            String input = sc.nextLine();
-            if (input.equals("exit")) {
+            String command = sc.nextLine();
+
+            if (command.equals("exit")) {
                 break;
-            }
-
-            // Echo command
-            else if (input.startsWith("echo ")) {
-                System.out.println(input.substring(5));
-
-            } else if (input.startsWith("type ")) {
-                if (input.substring(5).equals("echo") || input.substring(5).equals("exit")
-                        || input.substring(5).equals("type")) {
-                    System.out.println(input.substring(5) + " is a shell builtin");
-                } else {
-                    System.out.println(input.substring(5) + ": not found");
-                }
+            } else if (command.startsWith("echo")) {
+                System.out.println(command.substring(5));
+            } else if (command.startsWith("type")) {
+                String typeArg = command.substring(5);
+                System.out.println(type(typeArg));
             } else {
-                System.out.println(input + ": command not found");
+                System.out.println(command + ": command not found");
             }
-
         }
+
+        sc.close();
+    }
+
+    public static String type(String command) {
+        String[] commands = { "exit", "echo", "type" };
+        String path = System.getenv("PATH");
+        String[] pathDirs = path.split(":");
+
+        boolean isBuiltIn = false;
+        for (int i = 0; i < commands.length; i++) {
+            if (commands[i].equals(command)) {
+                return command + " is a shell builtin";
+            }
+        }
+
+        for (int i = 0; i < pathDirs.length; i++) {
+            File file = new File(pathDirs[i], command);
+            if (file.exists() && file.canExecute()) {
+                return command + " is " + file.getAbsolutePath();
+            }
+        }
+
+        return command + ": not found";
     }
 }
