402af1c23902b7774025eb17f9010ed667982fba
Priyansh2025 <priyansh05khanna@gmail.com>
2026-06-13 22:34:48 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
-        System.out.print("$ ");
 
         Scanner sc = new Scanner(System.in);
         while (true) {
+            System.out.print("$ ");
             String input = sc.nextLine();
             System.out.print(input + ": command not found");
 
