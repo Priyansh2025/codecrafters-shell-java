dc28404c87d0b143472977cfbca0b2c4cf7ab3a5
Priyansh2025 <priyansh05khanna@gmail.com>
2026-06-18 12:34:21 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         boolean exit = false;
         Scanner sc = new Scanner(System.in);
+
         while (!exit) {
             System.out.print("$ ");
             String input = sc.nextLine();
@@ -22,41 +23,50 @@ public class Main {
                 System.out.println(result);
             else if (Objects.equals(command, "type"))
                 System.out.println(type(result));
+            else if (Objects.equals(command, "pwd"))
+                System.out.println(System.getProperty("user.dir"));
             else if (getExecutable(command) != null) {
                 Process process = Runtime.getRuntime().exec(input.split(" "));
                 process.getInputStream().transferTo(System.out);
             } else
                 System.out.println(input + ": command not found");
         }
+
         sc.close();
     }
 
     public static String type(String command) {
-        String[] commands = { "exit", "echo", "type" };
-        String path_commands = System.getenv("PATH");
-        String[] path_command = path_commands.split(":");
+        String[] commands = { "exit", "echo", "type", "pwd" };
+
+        String pathCommands = System.getenv("PATH");
+        String[] pathCommand = pathCommands.split(":");
+
         for (String text : commands) {
             if (Objects.equals(text, command))
                 return command + " is a shell builtin";
         }
-        for (String path : path_command) {
+
+        for (String path : pathCommand) {
             File file = new File(path, command);
             if (file.exists() && file.canExecute()) {
                 return command + " is " + file.getAbsolutePath();
             }
         }
+
         return command + ": not found";
     }
 
     public static String getExecutable(String command) {
-        String path_commands = System.getenv("PATH");
-        String[] path_command = path_commands.split(":");
-        for (String path : path_command) {
+        String pathCommands = System.getenv("PATH");
+        String[] pathCommand = pathCommands.split(":");
+
+        for (String path : pathCommand) {
             File file = new File(path, command);
             if (file.exists() && file.canExecute()) {
                 return file.getAbsolutePath();
             }
         }
+
         return null;
     }
 }
